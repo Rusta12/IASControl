68ebdf1c434a4fac1feeb71ffbb0d42884993cea
rs.bulgakov@gmail.com <8ie9V4NR7yTGPsj>
2024-10-28 23:37:27 +0300

    upd report

--- reports/loader_fin.py
+++ reports/loader_fin.py
@@ -2,10 +2,12 @@ import time
 from settings.config import *
 
 
+url_report_fin = "https://ias.sport.mos.ru/Reports/ReportViewer.aspx?report_name=CompetitionCalendarSchoolSportFinans&report_param_backUrl=http://ias.sport.mos.ru"
+
 # Вход в отчет
 def come_fin_report(name, pass_name):
    #Вход в ИАС
-   driver.get("https://ias.sport.mos.ru/Reports/ReportViewer.aspx?report_name=CompetitionCalendarSchoolSportFinans&report_param_backUrl=http://ias.sport.mos.ru")
+   driver.get(url_report_fin)
    driver.implicitly_wait(30)
    login = driver.find_element(By.XPATH, "//input[@id='UserName']")
    login.send_keys(name)
@@ -15,11 +17,21 @@ def come_fin_report(name, pass_name):
    return
 
 
-def calendar_load(period, status):
+def calendar_period(period):
     time.sleep(1)
-    kalendar = driver.find_element(By.XPATH,"//select[@id='rvMain_ctl04_ctl03_ddValue']")
-    kalendar.send_keys(Keys.ENTER)
+    select_element = driver.find_element(By.XPATH,"//select[@id='rvMain_ctl04_ctl03_ddValue']")
+    select_element.send_keys(Keys.ENTER)
     driver.implicitly_wait(3)
+    select = Select(select_element)
+    for option in select.options:
+        if period in option.text:
+            select.select_by_visible_text(option.text)
+            print(f"Выбрана опция: {option.text}")
+            break
+
+
+
+
 
 
 
--- settings/config.py
+++ settings/config.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 import os
 import dotenv
 
