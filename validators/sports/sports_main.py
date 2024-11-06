994f6c5fa1a41b141385eb4c56adbbbd3cd33277
usevromoscow <bulgakov.rs@prom.moscow>
2024-11-06 17:15:35 +0300

    upd

--- validators/sports/sports_main.py
+++ validators/sports/sports_main.py
@@ -3,7 +3,7 @@ import re
 #Модули
 from validators.collectiv_def import input_report_text 
 from validators.collectiv_def import search_region, IASControl_comment
-from validators.collectiv_def import search_status_sports
+from validators.collectiv_def import search_status_sports, search_status_ytm, search_years_athletes
 
 
 def filtred_complex_event(df, excel_file_path):
@@ -17,8 +17,20 @@ def filtred_complex_event(df, excel_file_path):
     input_report_text(df_filtered, excel_file_path, report_text)
     return
 
-#Общая проверка по отвественным.
+def filtred_ytm_event(df, excel_file_path):
+    name_status = '|'.join(search_status_ytm)
+    name_age = '|'.join(search_years_athletes)
+    df_filtered = df[
+        df['Статус мероприятия'].str.contains(name_status, case=False, na=False) &
+        df['Пол/Возраст'].str.contains(name_age, case=False, na=False)
+    ]
+    report_text = IASControl_comment['sports'][1]
+    input_report_text(df_filtered, excel_file_path, report_text)
+    return
+
+#Общая проверка
 def sports_main_concat(df, excel_file_path):
     filtred_complex_event(df, excel_file_path)
+    filtred_ytm_event(df, excel_file_path)
     print('Общая проверка по Виду спорта успешно пройдена!')
     return
